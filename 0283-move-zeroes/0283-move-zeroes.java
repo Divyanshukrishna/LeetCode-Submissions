54fdd4340236f90e94cdc1a559953eeaad59d75f
DIVYANSHU_KRISHNA <131163849+Divyanshukrishna@users.noreply.github.com>
2024-08-29 15:43:20 +0530

    newApproach

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int lastNonZeroFoundAt = 0;
+        int count = 0;
         
-        // First pass: move all non-zero elements to the front
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt++] = nums[i];
+                nums[count++] = nums[i];
             }
         }
         
-        // Second pass: fill the remaining positions with zeros
-        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
-            nums[i] = 0;
+        while(count < nums.length) {
+            nums[count++] = 0;
         }
     }
 }
