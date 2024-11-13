2143788d9db952c09313a88a2bfacbb30316a761
DIVYANSHU_KRISHNA <131163849+Divyanshukrishna@users.noreply.github.com>
2024-11-13 19:38:35 +0530

    Time: 2 ms (97.2%), Space: 41.9 MB (18.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2 == 1) return false;
-        char[] stack = new char[s.length() +2];
-        int pointer = 0;
-        int len = s.length();
-        for(char a : s.toCharArray()){
-            if(pointer >= len) return false;
-            // System.out.println(pointer);
-            switch(a){
-                case ')':
-                    if(pointer == 0 || stack[pointer] != '(') return false;
-                    pointer--;
-                    break;
-                case '}':
-                    if(pointer == 0 ||  stack[pointer] != '{') return false;
-                    pointer--;
-                    break;
-                case ']':
-                    if(pointer == 0 ||  stack[pointer] != '[') return false;
-                    pointer--;
-                    break;
-                default:
-                    // System.out.println(pointer+1);
-                    // pointer++;
-                    stack[++pointer] = a;
+        Stack<Character> str = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                str.push(ch);
+            }else{
+                if(str.isEmpty()){
+                    return false;
+                }
+                if((str.peek() == '[' && ch == ']') || (str.peek() == '{' && ch == '}') || 
+                (str.peek() == '(' && ch == ')')){
+                    str.pop();
+                }else{
+                    return false;
+                }
             }
         }
-        // System.out.println(pointer);
-        return pointer == 0;
+        if(str.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
