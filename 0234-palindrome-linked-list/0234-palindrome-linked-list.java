645f8d3384c3395cb613ba4498671c74fd905b64
DIVYANSHU_KRISHNA <131163849+Divyanshukrishna@users.noreply.github.com>
2025-07-21 19:15:20 +0530

    Time: 3 ms (99.7%), Space: 69 MB (16.33%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,20 +12,24 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast!=null && fast.next != null){
+
+        while(fast!=null && fast.next!=null){ // finding the middle element of the linkedlist
             slow = slow.next;
             fast = fast.next.next;
         }
 
         ListNode prev = null;
-        while(slow!= null){
-            ListNode temp = slow.next;
+        ListNode front = null;
+        while(slow!=null){
+            front = slow.next;
             slow.next = prev;
             prev = slow;
-            slow = temp;
+            slow = front;
         }
+
         while(prev!=null){
             if(head.val != prev.val) return false;
+
             head = head.next;
             prev = prev.next;
         }
